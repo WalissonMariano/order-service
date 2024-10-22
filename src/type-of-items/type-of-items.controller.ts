a06eea38dd22b900c738e066574d5207edc42762
Walisson Mariano <marianowalisson98@gmail.com>
2024-10-21 21:48:35 -0300

    feat: create endpoint getAllTypesOfItems

--- src/type-of-items/type-of-items.controller.ts
+++ src/type-of-items/type-of-items.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ReturnTypeOfItemsDto } from './dto/return-type-of-items.dto';
+import { TypeOfItemsService } from './type-of-items.service';
 
 @Controller('type-of-items')
-export class TypeOfItemsController {}
+export class TypeOfItemsController {
+    constructor(
+        private readonly typeOfCategoryService: TypeOfItemsService,
+    ) {}
+
+    @Get()
+    async getAllTypesOfItems(): Promise<ReturnTypeOfItemsDto[]> {
+        return (await this.typeOfCategoryService.getAllTypesOfItems()).map(
+            (typeOfItems) => new ReturnTypeOfItemsDto(typeOfItems),
+        );
+    }
+}
--- src/type-of-items/type-of-items.service.ts
+++ src/type-of-items/type-of-items.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TypeOfItems } from './entities/type-of-items.entity';
 
 @Injectable()
-export class TypeOfItemsService {}
+export class TypeOfItemsService {
+    constructor(
+        @InjectRepository(TypeOfItems)
+        private readonly typeOfItemsRepository: Repository<TypeOfItems>,
+    ) {}
+
+    async getAllTypesOfItems(): Promise<TypeOfItems[]> {
+        const typeOfItems = await this.typeOfItemsRepository.find();
+
+        if (!typeOfItems || typeOfItems.length === 0) {
+            throw new NotFoundException(`type of items empty`);
+        }
+
+        return typeOfItems;
+    }
+}
