b584a3de34fb76537eecc4ce75858fc080193bb7
Walisson Mariano <marianowalisson98@gmail.com>
2024-10-26 16:06:30 -0300

    feat: create migration table order_items

--- src/migration/1729967663182-create-table-order-items.ts
+++ src/migration/1729967663182-create-table-order-items.ts
@@ -5,35 +5,35 @@ export class CreateTableOrderItems1729967663182 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
         
-            CREATE TABLE public.item (
+            CREATE TABLE public.order_items (
                     id integer NOT NULL,
-                    number_item integer NOT NULL,
-                    type_of_item_id integer NOT NULL,
-                    item_description character varying NOT NULL,
-                    unit_value double precision NOT NULL,
-                    image character varying NOT NULL,
+                    order_id integer NOT NULL,
+                    item_id integer NOT NULL,
+                    quantity double precision NOT NULL,
+                    tax_value double precision NOT NULL,
+                    total_value double precision NOT NULL,
                     created_at timestamp without time zone DEFAULT now() NOT NULL,
                     update_at timestamp without time zone DEFAULT now() NOT NULL,
                     PRIMARY KEY (id)
                 );
             
-            CREATE SEQUENCE public.item_id_seq
+            CREATE SEQUENCE public.order_items_id_seq
                 INCREMENT 1
                 START 1
                 MINVALUE 1
                 MAXVALUE 2147483647
                 CACHE 1;
                 
-            ALTER SEQUENCE public.item_id_seq OWNED BY public.item.id;
+            ALTER SEQUENCE public.order_items_id_seq OWNED BY public.order_items.id;
     
-            ALTER TABLE ONLY public.item ALTER COLUMN id SET DEFAULT nextval('public.item_id_seq'::regclass);
+            ALTER TABLE ONLY public.order_items ALTER COLUMN id SET DEFAULT nextval('public.order_items_id_seq'::regclass);
     
             `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         queryRunner.query(`
-            DROP TABLE public.type_of_items
+            DROP TABLE public.order_items
             `);
     }
 
